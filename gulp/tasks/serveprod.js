e8c66715713b1fed5addee3d8c2ec95bf4e1be62
Wei Lu <iamweilu@gmail.com>
2017-05-25 22:22:53 +1000

    update deployment script.

--- gulp/tasks/serveprod.js
+++ gulp/tasks/serveprod.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
+var connect = require('gulp-connect');
 
-gulp.task('serveprod', function() {
-  connect.server({
-    root: laosgps,
+gulp.task('serveprod', function(){
+	connect.server({
+    root: ['build'],
     port: process.env.PORT || 5000, // localhost:5000
     livereload: false
-  });
-});
+	});
+});	
